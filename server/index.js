4db7bd3e6facb77df30b8437f546d0206f002531
pammu453 <111453798+pammu453@users.noreply.github.com>
2024-01-20 22:48:22 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch((error) => {
+        console.log(error.message);
+    });
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000!");
+});
