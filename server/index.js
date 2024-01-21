438bc92a3abc76e5bde8eccce18ad23354a1d844
pammu453 <111453798+pammu453@users.noreply.github.com>
2024-01-21 23:05:32 +0530

    Create signup API route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
 dotenv.config();
 
 const app = express();
@@ -19,4 +20,7 @@ app.listen(5000, () => {
     console.log("Server is running on port 5000!");
 });
 
+app.use(express.json())
+
+app.use("/api/auth",authRouter )
 app.use("/api/user", userRouter)
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -14,11 +14,6 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
     },
 }, { timestamps: true })
 
