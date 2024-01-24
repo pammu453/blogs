c2f7492a058f6226e7b26eb0c690b30a26f92fee
pammu453 <111453798+pammu453@users.noreply.github.com>
2024-01-24 19:35:09 +0530

    Create signin API route

--- server/controllers/auth.contoler.js
+++ server/controllers/auth.contoler.js
@@ -1,6 +1,9 @@
 import bcrypt from 'bcryptjs'
 import User from "../models/user.model.js"
 import { errorHandler } from '../utlis/error.js'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config();
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body
@@ -25,6 +28,38 @@ export const signup = async (req, res, next) => {
     try {
         await newUser.save()
         res.json("SignUp succefull")
+    } catch (error) {
+        next(error)
+    }
+}
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body
+
+    if (!email || !password || email === "" || password === "") {
+        next(errorHandler(400, "All fields are required"))
+    }
+
+    try {
+        const validUser = await User.findOne({ email })
+        if (!validUser) {
+            return next(errorHandler(404, "User not found"))
+        }
+        const validPassword = bcrypt.compareSync(password, validUser.password)
+
+        if (!validPassword) {
+            return next(errorHandler(404, "Invalid credentials"))
+        }
+
+        const token = jwt.sign(
+            { userId: validUser._id }, process.env.JWT_KEY, { expiresIn: "2d" }
+        )
+
+        const { password: pass, ...rest } = validUser._doc
+
+        res.status(200).cookie("access_token", token, {
+            httpOnly: true
+        }).json(rest)
+
     } catch (error) {
         next(error)
     }
