ea9088dc8ca040832841ebb1b58774cdb995084c
pammu453 <111453798+pammu453@users.noreply.github.com>
2024-02-09 19:37:58 +0530

    Add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -8,12 +8,14 @@ import {
 } from "flowbite-react"
 import { PiGitlabLogoFill } from "react-icons/pi"
 import { Link, useNavigate } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux";
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
 
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+  const { loading, error } = useSelector((state) => state.user)
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value })
@@ -22,9 +24,7 @@ const SignIn = () => {
   const handleSubmite = async (e) => {
     e.preventDefault()
     try {
-      setLoading(true)
-      setError(null)
-
+      dispatch(signInStart())
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: { "Content-Type": 'application/json' },
@@ -35,15 +35,13 @@ const SignIn = () => {
       const data = await res.json()
 
       if (data.success === false) {
-        setLoading(false)
-        return setError(data.message)
+        return dispatch(signInFailure(data.message))
       }
 
-      setLoading(false)
+      dispatch(signInSuccess(data))
       navigate("/")
     } catch (error) {
-      setError(error.message)
-      setLoading(false)
+      dispatch(signInFailure(error.message))
     }
   }
 
