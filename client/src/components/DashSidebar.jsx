0a9fe24dde0db53fc6b94c9ad83b104da23dd0fa
pammu453 <111453798+pammu453@users.noreply.github.com>
2024-02-12 11:59:05 +0530

    Complete profile page UI

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -16,11 +16,11 @@ const DashSidebar = () => {
         <Sidebar className='w-full md:w-56'>
             <Sidebar.Items>
                 <Sidebar.ItemGroup>
-                    <Link to={'/dashboard?tab=profile'}>
-                        <Sidebar.Item active={tab === "profile"} icon={HiUser} label={"User"} labelColor="dark">
+                   
+                        <Sidebar.Item to={'/dashboard?tab=profile'} active={tab === "profile"} icon={HiUser} label={"User"} labelColor="dark">
                             Profile
                         </Sidebar.Item>
-                    </Link>
+                   
                     <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
                         Sign Out
                     </Sidebar.Item>
