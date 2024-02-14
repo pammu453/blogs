8743a33d637c3fd8ade3c958a30d8b292d68ecc1
pammu453 <111453798+pammu453@users.noreply.github.com>
2024-02-14 22:11:27 +0530

    Show user posts inside dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,10 +1,82 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Table, Spinner, Avatar } from 'flowbite-react'
+import { Link } from 'react-router-dom'
 
 const DashPosts = () => {
+  const { currentUser } = useSelector(state => state.user)
+  const [userPosts, setUserPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        setLoading(true)
+        const res = await fetch(`/api/post/getPosts?userId=${currentUser._id}`)
+        const data = await res.json()
+        if (res.ok) {
+          setLoading(false)
+          setUserPosts(data.posts)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getPosts()
+  }, [currentUser._id])
+
   return (
-    <div>
-      DashPosts
-    </div>
+    <div className="table-auto overflow-x-auto p-2 w-full">
+      {
+        userPosts.length > 0 ? (
+          <>
+            <Table>
+              <Table.Head>
+                <Table.HeadCell>Date Updated</Table.HeadCell>
+                <Table.HeadCell>Image</Table.HeadCell>
+                <Table.HeadCell>Title</Table.HeadCell>
+                <Table.HeadCell>Category</Table.HeadCell>
+                <Table.HeadCell>Delete</Table.HeadCell>
+                <Table.HeadCell>Edit</Table.HeadCell>
+              </Table.Head>
+              {
+                userPosts && userPosts.map((post) => (
+                  <Table.Body key={post._id} className="divide-y p-2">
+                    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                      <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                        {(post.updatedAt).substring(0, 10)}
+                      </Table.Cell>
+                      <Table.Cell>
+                        <Link to={`${post.slug}`}>
+                          <Avatar img={post.image} bordered size="md" className='float-start' />
+                        </Link>
+                      </Table.Cell>
+                      <Table.Cell>
+                        <Link to={`${post.slug}`}>
+                          {post.title}
+                        </Link>
+                      </Table.Cell>
+                      <Table.Cell>{post.category}</Table.Cell>
+                      <Table.Cell className='text-red-600 hover:underline cursor-pointer'>Delete</Table.Cell>
+                      <Table.Cell>
+                        <Link to={`/update-post/${post._id}`} className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                          Edit
+                        </Link>
+                      </Table.Cell>
+                    </Table.Row>
+                  </Table.Body>
+                ))
+              }
+            </Table >
+          </>
+        ) : (
+          <p className='text-center mt-10' hidden={loading}>You don't have the posts now </p>
+        )
+      }
+      {loading && <div className="text-center w-full mt-5">
+        <Spinner aria-label="Center-aligned spinner Extra large example" size="xl" />
+      </div>}
+    </div >
   )
 }
 
--- server/controllers/post.contoler.js
+++ server/controllers/post.contoler.js
@@ -33,7 +33,7 @@ export const createPost = async (req, res, next) => {
 export const getPosts = async (req, res, next) => {
     try {
         const startIndex = parseInt(req.query.startIndex) || 0
-        const limit = parseInt(req.query.limit) || 2
+        const limit = parseInt(req.query.limit) || 9
         const sortDirection = req.query.order === "asc" ? 1 : -1
         const posts = await Post.find({
             ...(req.query.userId && { userId: req.query.userId }),
