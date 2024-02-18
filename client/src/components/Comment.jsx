5cb35ca68e60b2b1ece5d04050e3432ab8c1b32e
pammu453 <111453798+pammu453@users.noreply.github.com>
2024-02-18 10:27:51 +0530

    Styled comment component

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -20,15 +20,17 @@ const Comment = ({ comment }) => {
     }, [comment])
 
     return (
-        <div className='flex items-start p-4 border-b dark:text-slate-200 flex-col'>
-            <div className='flex-shrink-0 mr-4'>
-                <img className='w-10 h-10 rounded-full bg-gray-200' src={user.profilePicture || "https://tse1.mm.bing.net/th?id=OIP.wMgXjlCbLG7CIkCOUnuoYQAAAA&pid=Api&P=0&h=180"} alt={user.username} />
-            </div>
-            <div className="flex-grow sm:mt-2 sm:w-full">
-                <div className="flex mb-1 sm:flex-col sm:items-start sm:w-full">
-                    <span className='font-bold mr-1 text-sm truncate'>{user && user.username ? `@${user.username}` : "@anonymous user"}</span>
-                    <p className='text-gray-500 text-xs'>{moment(comment.createdAt).fromNow()}</p>
+        <div className="flex flex-col  p-4 border-b">
+            <div className='flex flex-row items-start dark:text-slate-200'>
+                <div className='flex-shrink-0 mr-4'>
+                    <img className='w-10 h-10 rounded-full bg-gray-200' src={user.profilePicture || "https://tse1.mm.bing.net/th?id=OIP.wMgXjlCbLG7CIkCOUnuoYQAAAA&pid=Api&P=0&h=180"} alt={user.username} />
                 </div>
+                <div className="flex flex-col mb-1">
+                    <span className='font-bold mr-1 text-sm truncate'>{user && user.username ? `@${user.username}` : "@anonymous user"}</span>
+                    <span className='text-gray-500 text-xs'>{moment(comment.createdAt).fromNow()}</span>
+                </div>
+            </div>
+            <div className="flex-grow block sm:mt-2 sm:w-full">
                 <p className='block'>{comment.content}</p>
             </div>
         </div>
